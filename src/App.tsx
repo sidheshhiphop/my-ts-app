b3024848adbdea7e0e0fc49bb1043c18bbafeb4b
sidheshhiphop <json.stringify.sid@gmail.com>
2024-02-09 03:27:42 +0530

    kk

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,7 @@ const App: React.FC = () => {
           </div>
         </div>
         <div className="mt-8 flex justify-center text-lg text-black">
-          <div onClick={handleRefreshClick}  className="rounded-3xl bg-lime-500  px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-lime-700">Refresh</div>
+          <div onClick={handleRefreshClick}  className="rounded-3xl cursor-pointer bg-lime-500  px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-lime-700">Refresh</div>
         </div>
       </form>
     </div>
